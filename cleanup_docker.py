06e88ed165ab472f58d397b780abeabd0931d0f9
titesenal@gmail.com <patrick.hirata@m4u.com.br>
2017-02-15 15:45:48 -0200

    initial commit

--- cleanup_docker.py
+++ cleanup_docker.py
@@ -5,6 +5,8 @@ import json
 import os
 
 # Clean Exited/Unused Containers
+
+
 def cleanContainer():
     print "##### Containers #####"
     client = docker.from_env()
@@ -16,41 +18,49 @@ def cleanContainer():
         print status
         if (status == 'exited'):
             print "Removing Exited Container"
-            # container.remove()
-
-    # captura e atribui a data de inicio de fim do container
+            container.stop()
+            container.remove()
+        # captura e atribui a data de inicio de fim do container
         initdate_container = data['State']['StartedAt']
         enddate_container = data['State']['FinishedAt']
-        print initdate_container
-        print enddate_container
+        #print initdate_container
+        #print enddate_container
 
 cleanContainer()
 
-'''
+
 def stopContainer():
     client = docker.from_env()
     for container in client.containers.list(all):
         data = container.attrs
         status_running = data['State']['Running']
         status_pause = data['State']['Paused']
-
-'''
-
+        if (status_running == 'true'):
+            if (status_pause == true):
+                print "Stopping Container"
+                container.stop()
 
 print ""
 
 # Clean Untagged Images
+
+
 def cleanImages():
     print "##### Images #####"
     client = docker.from_env()
     for image in client.images.list():
         data = image.attrs
-        isUntagged = data['RepoTags']
-        print image.short_id
-        print isUntagged
-        if (isUntagged == [u'<none>:<none>']):
-            stopContainer();
+        image_name = image
+        tag = data['RepoTags']
+        print image_name
+        print tag
+        if (tag == None):
+            stopContainer()
             print "Image is Untagged"
-            # os.system('docker rmi %s" %')
+            # client.images.remove(image)
+            os.system('docker rmi %s' % image.id)
+        #else:
+        #    print "Image Ok"
+
 
 cleanImages()
