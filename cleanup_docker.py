32d39df80222bfa12bfef776793ae0d72a978aed
titesenal@gmail.com <patrick.hirata@m4u.com.br>
2017-02-16 15:17:27 -0200

    comentando stopContainer()

--- cleanup_docker.py
+++ cleanup_docker.py
@@ -4,12 +4,11 @@ import docker
 import json
 import os
 
+client = docker.from_env()
+
 # Clean Exited/Unused Containers
-
-
 def cleanContainer():
     print "##### Containers #####"
-    client = docker.from_env()
     for container in client.containers.list(all):
         data = container.attrs
         print container.short_id
@@ -27,40 +26,36 @@ def cleanContainer():
         #print enddate_container
 
 cleanContainer()
-
-
+'''
 def stopContainer():
-    client = docker.from_env()
     for container in client.containers.list(all):
         data = container.attrs
         status_running = data['State']['Running']
         status_pause = data['State']['Paused']
         if (status_running == 'true'):
-            if (status_pause == true):
+            if (status_pause == 'true'):
                 print "Stopping Container"
                 container.stop()
-
+'''
 print ""
 
 # Clean Untagged Images
-
-
 def cleanImages():
     print "##### Images #####"
-    client = docker.from_env()
     for image in client.images.list():
         data = image.attrs
-        image_name = image
         tag = data['RepoTags']
-        print image_name
+        print image
         print tag
         if (tag == None):
-            stopContainer()
-            print "Image is Untagged"
-            # client.images.remove(image)
-            os.system('docker rmi %s' % image.id)
-        #else:
-        #    print "Image Ok"
-
+           #stopContainer()
+           print "Image is Untagged"
+           # client.images.remove(image)
+           os.system('docker rmi -f %s' % image.id)
+        if(tag == [u'<none>:<none>']):
+           #stopContainer()
+           print "Image is Untagged"
+           # client.images.remove(image)
+           os.system('docker rmi -f %s' % image.id)
 
 cleanImages()
